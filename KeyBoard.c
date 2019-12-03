d9ed4722e2a89a770039c32de9a61e0beefe2562
Faizan-Mushtaq <faizan.wani234@gmail.com>
2019-12-04 00:07:54 +0530

    add comments

--- KeyBoard.c
+++ KeyBoard.c
@@ -13,9 +13,9 @@ sbit col2=P1^1;
 sbit col3=P1^0;
 
 tbyte code keys[4][4]={{'0','1','2','3'},
-											 {'4','5','6','7'},
-											 {'8','9','A','B'},
-											 {'C','D','E','F'}};
+					  {'4','5','6','7'},
+					  {'8','9','A','B'},
+					  {'C','D','E','F'}};
 
 main()
 {
@@ -36,19 +36,19 @@ main()
 			if(col1==0){colpos=1;break;}
 			if(col2==0){colpos=2;break;}
 			if(col3==0){colpos=3;break;}
-			
+
 			row0=1;row1=0;row2=1;row3=1;rowpos=1;
 			if(col0==0){colpos=0;break;}
 			if(col1==0){colpos=1;break;}
 			if(col2==0){colpos=2;break;}
 			if(col3==0){colpos=3;break;}
-			
+
 			row0=1;row1=0;row2=0;row3=1;rowpos=2;
 			if(col0==0){colpos=0;break;}
 			if(col1==0){colpos=1;break;}
 			if(col2==0){colpos=2;break;}
 			if(col3==0){colpos=3;break;}
-			
+
 			row0=1;row1=1;row2=1;row3=0;rowpos=3;
 			if(col0==0){colpos=0;break;}
 			if(col1==0){colpos=1;break;}
@@ -56,10 +56,10 @@ main()
 			if(col3==0){colpos=3;break;}
 		}
 		delayms(20); //debounce
-		P0=keys[rowpos][colpos];
+		P0=keys[rowpos][colpos]; //output on LEDs
 		delayms(1000);
-		
-		SBUF=keys[rowpos][colpos];
+
+		SBUF=keys[rowpos][colpos]; // serially to terminal
 		while(col0==0 || col1==0 || col2==0 || col3==0);
 		delayms(20);
 	}
